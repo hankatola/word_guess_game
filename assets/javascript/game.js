65534e47b360502cb457e13051e93cd10de45854
dPaq <dpaquette2.0@gmail.com>
2018-12-26 21:07:44 -0500

    js logic done, html bones started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+
 /*
     Word Guess Game
     Author: Dave Paquette
@@ -20,7 +21,7 @@
 
 
 // Memory objects
-const lives = 12;
+const lives = 12
 var game = {
     word: [],                   // exploded word from wordBank goes here
     score: [],                  // len(score) = len(word), either 0 or 1 depending on whether or not the letter has been found
@@ -28,181 +29,155 @@ var game = {
     remaining: lives,           // guesses remaining. g/o when remaining == 0
     display: [],                // display variable
 }
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+var wordBank = [
+    'worthless',
+    'decorate',
+    'want',
+    'copy',
+    'trouble',
+    'clean',
+    'plastic',
+    'premium',
+    'measure',
+    'drop',
+    'practise',
+    'brush',
+    'spoon',
+    'crack',
+    'mere',
+    'shape',
+    'bridge',
+    'used',
+    'troubled',
+    'fixed',
+    'statement',
+    'wrathful',
+    'yielding',
+    'sound',
+    'acoustics',
+    'transport',
+    'aggressive',
+    'lovely',
+    'deer',
+    'oil',
+    'marble',
+    'smoke',
+    'travel',
+    'change',
+    'pancake',
+    'belong',
+    'hellish',
+    'letter',
+    'eyes',
+    'shame',
+    'futuristic',
+    'grieving',
+    'extend',
+    'spiky',
+    'automatic',
+    'unsuitable',
+    'bawdy',
+    'suck',
+    'elastic',
+    'tease'
+]
 
+// functions
+function randInt() {
+    // generates a random number in [0:49]
+    let x = Math.ceil(Math.random() * wordBank.length) - 1;
+    return x;
+}
 
+function prepareWord() {
+    game.word = wordBank[randInt()].split('')
+    game.tries = []
+    game.score = []
+    game.display = []
+    game.remaining = lives;
+    for (let i in game.word) {
+        game.score.push(0)
+        game.display.push('_')
+    }
+}
+
+function sum() {
+    // sums game.score to check for victory condition
+    function add(x, y) {
+        return x + y;
+    }
+    return game.score.reduce(add, 0);
+}
+
+function updateGame(key) {
+    // record k, update remaining guesses & score if k in word
+    if (game.tries.indexOf(key) < 0) {
+        game.tries.push(key)
+    } else {
+        return
+    }
+    if (game.word.indexOf(key) >= 0) {
+        for (let i in game.word) {
+            if (key == game.word[i]) {
+                game.score[i] = 1;
+                game.display[i] = game.word[i];
+            }
+        }
+    } else {
+        game.remaining -= 1
+    }
+}
+
+function gameState() {
+    // checks for victory or defeat
+    let s = 0;
+    if (game.word.length === sum()) {
+        // victory
+        s = 1;
+    } else if (game.remaining === 0) {
+        // defeat
+        s = -1;
+    }
+    return s;
+}
+
+function keyPress(key) {
+    // checks validity of keypress & gets valid letter
+    let k = key.key.toLowerCase()
+    if (alphabet.indexOf(k) < 0) {
+        return;
+    }
+    main(k)
+}
 
 /*
     Main
     ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 */
 function main(key) {
-
-    // Private Variables
     let k, gs;
-    let wordBank = [
-        'worthless',
-        'decorate',
-        'want',
-        'copy',
-        'trouble',
-        'clean',
-        'plastic',
-        'premium',
-        'measure',
-        'drop',
-        'practise',
-        'brush',
-        'spoon',
-        'crack',
-        'mere',
-        'shape',
-        'bridge',
-        'used',
-        'troubled',
-        'fixed',
-        'statement',
-        'wrathful',
-        'yielding',
-        'sound',
-        'acoustics',
-        'transport',
-        'aggressive',
-        'lovely',
-        'deer',
-        'oil',
-        'marble',
-        'smoke',
-        'travel',
-        'change',
-        'pancake',
-        'belong',
-        'hellish',
-        'letter',
-        'eyes',
-        'shame',
-        'futuristic',
-        'grieving',
-        'extend',
-        'spiky',
-        'automatic',
-        'unsuitable',
-        'bawdy',
-        'suck',
-        'elastic',
-        'tease'
-    ]
-    let alphabet = [
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'q',
-        'r',
-        's',
-        't',
-        'u',
-        'v',
-        'w',
-        'x',
-        'y',
-        'z'
-    ]
-
-    // Private Functions
-    function keyPress(key) {
-        // checks validity of keypress & gets valid letter
-        let k = parseInt(key.keyCode) - 65;
-        if (k > 25 || k < 0) {
-            return;
-        }
-        return alphabet[k];
+    if (game.word.length === 0) {
+        prepareWord()
     }
-
-    function randInt() {
-        // generates a random number in [0:49]
-        let x = Math.ceil(Math.random() * wordBank.length) - 1;
-        return x;
+    if (key != undefined) {
+        updateGame(key)
     }
-
-    function reset() {
-        // draws & prepares new word & resets game
-        game.word = wordBank[randInt()].split('');
-        game.tries = [];
-        game.score = [];
-        game.remaining = lives;
-        for (let i in game.word) {
-            game.score.push(0);
-            game.display.push('_');
-        }
+    gs = gameState()
+    if (gs === 1) {
+        console.log('victory')
+        prepareWord()
     }
-
-    function sum() {
-        // sums game.score to check for victory condition    
-        function add(x, y) {
-            return x + y;
-        }
-        return game.word.reduce(add, 0);
+    if (gs === -1) {
+        console.log('You lose')
+        prepareWord()
     }
-
-    function gameState() {
-        // checks for victory or defeat
-        let s = 0;
-        if (game.word.length === sum()) {
-            // victory
-            s = 1;
-        } else if (game.remaining === 0) {
-            // defeat
-            s = -1;
-        }
-        return s;
-    }
-
-    function updateGame() {
-        // record k, update remaining guesses & score if k in word
-        game.tries.push(k);
-        game.remaining -= 1;
-        if (game.word.indexOf(k) < 0) {
-            for (let i in game.word) {
-                if (k == game.word[i]) {
-                    game.score[i] = 1;
-                    game.display[i] = game.word[i];
-                }
-            }
-        }
-    }
-
-    /*  Body
-        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾*/
-    if (game.remaining === lives) {                         // if true a new game is required
-        reset();
-    }
-    k = keyPress(key);
-    if (game.tries.indexOf(k) !== -1) {                     // duplicate keypress -> exit main
-        return;
-    }
-    updateGame();
-    gs = gameState();
-    if (gs === 0) {                                         // game not won or lost -> exit main
-        return
-    } else if (gs < 0) {                                    // defeat
-        // sadness
-    } else {                                                // victory
-        // celebration
-    }
-    game.remaining = lives;                                 // game's over & this causes reset on next go-around
+    console.log(game)
 }
 
+// Call main to prepare memory
+main()
+
+console.log(game)
 // Call main on keypress
-window.addEventListener('keyup', main, false);
+$(document).keyup(keyPress)
